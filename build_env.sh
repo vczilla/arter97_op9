fe6bd72d3d44e10b5a12d05d14db81472f68ca8e
vczilla <vczilla@gmail.com>
2022-02-04 08:21:12 +0000

    Turns out this kernel works with non OOS android builds. Plus from what I've seen the kernel used by OnePlus for OxygenOS 12 is incredibly similar to RealMe's kernel for their RealMe GT and another device of theirs that uses a different 5.4 build (check their Github's and you'll get what I mean). In the meantime fuck that shit I'd rather have a modifiable OS than no kernel source whatsoever. Especially considering the dirty trick pulled by both RealMe and Oppo regarding soft bricked devices. Which is easy enough to on ColorOS. And for which you're basically screwed unless you want to front the shillings for that damn MSM scam. Fortunately OnePlus was wise enough not to full for it and to still maintain a somewhat developer friendly attitude. Because that used to be their most endearing selling point. I tried my hand at Realme. Went thru 4 different RealMe GTs in the span of a month. Got two stolen while unconscious and brought over to a hospital. Bricked the other two. Decided to try my hand at that online bullshit where you have to pay someone to unbrick it. You think they'd give you a temporary user account on their annoyingly limited version of MsmDownloadTool but oh no. They want to do it on TeamViewer. Which failed miserably after I fronted almost 3 times the amount of mullah asked for. Duh. My main (and only computer) is an Apple MacBook Air M1. So I actually had the dude try it on a Qemu virtual machine. At first a Windows 7 build. And then a Windows 10 one. Well yeah. Dude failed miserably . Decided to cut my losses . Got rid of them RealMe. Got me a Oneplus 9 Pro for Christmas. Which I bricked the first week.After I tried the ColorOs based OxygenOs 12 build. Which I unbricked using guess what? MsmDownload tool and the exact same Qemu Win7 virtual machine. I embrass change. I really tried my hand after having been a OnePlus customer for the last few years (OnePlus 5,Oneplus 6,OnePlus 7 Pro and now that OnePlus 9). My OnePlus 7 also got nicked while brought over to a hospital. Had an interesting October month. Spent my birthday in a hospital . I'm hijacking that commit to mention that I'm onto them. Considering the amount of people you can actually hire to do that unbrick crap that used to be free and that entails a modification of a tool they don't own (Qualcomm does) I'd bet most of them are actually staff members. Either that or their is a massive security flaw on their ill-thought out anti-customer self repair stuff. Plus I mentioned to that dude ( who is actually on telegram ) that it was probably a question of time before that annoying crap got hacked. Granted it's harder than just gaining access to a dodgy account to due them secure firehose loaders and that VIP programming stuff. But mark my words it's just a question of time.

--- build_env.sh
+++ build_env.sh
@@ -2,7 +2,7 @@ export KERNELDIR=`readlink -f .`
 export RAMFS_SOURCE=`readlink -f $KERNELDIR/ramdisk`
 export PARTITION_SIZE=134217728
 
-export OS="11.0.0"
-export SPL="2021-11"
+export OS="12.0.0"
+export SPL="2022-01"
 export VERSION="$(cat $KERNELDIR/version)-$(date +%F | sed s@-@@g)"
 echo version: $VERSION
